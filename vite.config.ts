20574ef35527816658a426e63580ad5349610de0
Akshay Kadambattu Nair <akshayakn6@gmail.com>
2024-01-28 18:23:04 -0500

    Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,56 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      "name": "Deckhouse",
+      "short_name": "DH",
+      "icons": [
+        {
+          "src": "/public/maskable_icon_x512.png",
+          "sizes": "200x200",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "/public/android-chrome-192x192.png",
+          "sizes": "144x144",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "/public/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "/public/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "/public/android-chrome-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png",
+          "purpose": "any"
+        },
+        {
+          "src": "/public/android-chrome-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png",
+          "purpose": "any"
+        }
+      ],
+      "theme_color": "#ffffff",
+      "background_color": "#ffffff",
+      "description": "DTMT",
+      "display": "standalone",
+      "scope": "/",
+      "start_url": "dtmt-project.web.app"
+    }
+  })]
 })
