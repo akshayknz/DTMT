ed82068f80068cb026e76ac0c5c0fd1c6e4d16a7
Akshay Kadambattu Nair <akshayakn6@gmail.com>
2024-01-29 08:20:57 -0500

    Interface organize

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,19 @@
 import { collection, addDoc, getDocs, doc, setDoc } from "firebase/firestore";
 import { db } from "./firebase/config";
 import firebase from "firebase/compat/app";
-
+import { UserProps } from "./interfaces/interfaces";
 
 export const getUsers = async (): Promise<any[]> => {
     const querySnapshot = await getDocs(collection(db, "Organizations"));
     
     return querySnapshot.docs.map((doc) => doc.data());
 };
-type SaveUserProps = {
-    displayName: string, email: string, photoURL: string, uid: string
-}
-export const saveUser = async ({displayName, email, photoURL, uid}): Promise<SaveUserProps> => {
+
+export const saveUser = async ({displayName, email, photoURL, uid}:UserProps): Promise<UserProps> => {
     const docId = doc(collection(db, "Organizations"))
     const querySnapshot = await setDoc(docId, {displayName:displayName, 
         email:email, photoURL: photoURL, uid: uid});
     console.log(querySnapshot, displayName, email, photoURL, uid);
     // return querySnapshot.docs.map((doc) => doc.data());
-    return {displayName, email, photoURL, uid};
+    return {displayName:displayName, email:email, photoURL:photoURL, uid:uid};
 };
--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -2,18 +2,25 @@ import { User } from 'firebase/auth'; //type User import
 export interface LoginFormValues {
     email: string;
     password: string;
-   }
-   
-   export interface UserFormValues {
+}
+
+export interface UserFormValues {
     email: string;
     password: string;
     displayName: string;
-   }
-   //IAuth context
-export  interface  IAuth {
-    user:  User  |  null;  //type User comes from firebase
-    loading:  boolean;
-    SignIn: (creds:  LoginFormValues) =>  void;
-    SignUp: (creds:  UserFormValues) =>  void;
-    SignOut: () =>  void;
-    }
+}
+//IAuth context
+export interface IAuth {
+    user: User | null;  //type User comes from firebase
+    loading: boolean;
+    SignIn: (creds: LoginFormValues) => void;
+    SignUp: (creds: UserFormValues) => void;
+    SignOut: () => void;
+}
+
+export interface UserProps {
+    displayName: string,
+    email: string,
+    photoURL: string,
+    uid: string
+}
